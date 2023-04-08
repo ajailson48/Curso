b9593c5166d393635adc37f4b3b39249d00d7661
Ajailson de Souza Castro <76658794+ajailson48@users.noreply.github.com>
2023-04-08 16:22:52 -0300

    ExerciciosPOO

---
+++ src/serasa/ExerciciosPOO/ContaCorrente.java
@@ -0,0 +1,18 @@
+package serasa.ExerciciosPOO;
+
+public class ContaCorrente {
+
+    
+	 public ContaCorrente(int i, int j) {
+    }
+
+    public boolean sacar(int i) {
+      return false;
+    }
+
+    public String getSaldo() {
+      return null;
+    }
+  
+    
+}
