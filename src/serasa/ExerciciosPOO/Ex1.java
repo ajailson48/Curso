6fc956ee5048737b317f38e4eacfee78628a88fc
Ajailson de Souza Castro <76658794+ajailson48@users.noreply.github.com>
2023-04-08 16:16:57 -0300

    ExerciciosPOO

---
+++ src/serasa/ExerciciosPOO/Ex1.java
@@ -0,0 +1,41 @@
+package ExerciciosPOO;
+
+public class Administrador {
+	
+	 public Administrador(String string, String string2, String string3, int i, int j, int k, int l) {
+	  }
+
+	  public String getNome() {
+	    return null;
+	  }
+
+	  public String getEndereco() {
+	    return null;
+	  }
+
+	  public String getTelefone() {
+	    return null;
+	  }
+
+	  public String getCodigoSetor() {
+	    return null;
+	  }
+
+	  public String getSalarioBase() {
+	    return null;
+	  }
+
+	  public String getImposto() {
+	    return null;
+	  }
+
+	  public String getAjudaDeCusto() {
+	    return null;
+	  }
+
+	  public String calcularSalario() {
+	    return null;
+	  }
+
+
+}
